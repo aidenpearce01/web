ec787003358b481a7d127bbb24e5530a36fb8bb4
Nguyen Hoang Minh <nghminh163@outlook.com>
2018-04-30 10:04:23 +0700

    Fix toggle 1

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_GET['a']==='true')) {
-	exec('mkdir test') : exec('rmdir test')
+if (isset($_GET['a'])) {
+    return $_GET['a'] == "true" ? exec('mkdir test') : exec('rmdir test');
 }
-if (isset($_GET['b']==='true')) {
-	exec('mkdir test1') : exec('rmdir test1')
-}
+//if (isset($_GET['b'] === 'true')) {
+//    exec('mkdir test1') : exec('rmdir test1')
+//}
 ?>
