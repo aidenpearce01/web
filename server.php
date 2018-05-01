baf601f1c18160edce2c28acf10db34b23c893ed
Aiden Pearce <32305640+aidenpearce01@users.noreply.github.com>
2018-05-01 15:24:49 +0700

    #1

--- server.php
+++ server.php
@@ -3,6 +3,7 @@ if (isset($_GET['a'])) {
     return $_GET['a'] == "true" ? exec('mkdir test') : exec('rmdir test');
 } elseif (isset($_GET['b'])) {
     return $_GET['b'] == "true" ? exec('mkdir test1') : exec('rmdir test1');
+}
 //if (isset($_GET['b'] === 'true')) {
 //    exec('mkdir test1') : exec('rmdir test1')
 //}
